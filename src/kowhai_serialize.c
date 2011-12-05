525377208f2752d78844a6b4552199041d5efc9d
Daniel Newton <djpnewton@gmail.com>
2011-12-06 00:43:56 +1300

    serialize leaf node array values

--- src/kowhai_serialize.c
+++ src/kowhai_serialize.c
@@ -18,6 +18,56 @@ int debug_printf(char* buf, size_t buf_size, char* format, ...)
     va_end(args);
 }
 
+int add_string(char** dest, size_t* dest_size, int* current_offset, char* string)
+{
+    int chars = snprintf(*dest, *dest_size, string);
+    if (chars >= 0)
+    {
+        *dest += chars;
+        *dest_size -= chars;
+        *current_offset += chars;
+    }
+    return chars;
+}
+
+int add_value(char** dest, size_t* dest_size, int* current_offset, uint16_t node_type, void* data)
+{
+    int chars;
+    switch (node_type)
+    {
+        case KOW_INT8:
+            chars = snprintf(*dest, *dest_size, "%d", *((int8_t*)data));
+            break;
+        case KOW_INT16:
+            chars = snprintf(*dest, *dest_size, "%d", *((int16_t*)data));
+            break;
+        case KOW_INT32:
+            chars = snprintf(*dest, *dest_size, "%d", *((int32_t*)data));
+            break;
+        case KOW_UINT8:
+            chars = snprintf(*dest, *dest_size, "%d", *((uint8_t*)data));
+            break;
+        case KOW_UINT16:
+            chars = snprintf(*dest, *dest_size, "%d", *((uint16_t*)data));
+            break;
+        case KOW_UINT32:
+            chars = snprintf(*dest, *dest_size, "%d", *((uint32_t*)data));
+            break;
+        case KOW_FLOAT:
+            chars = snprintf(*dest, *dest_size, "%f", *((float*)data));
+            break;
+        default:
+            return -1;
+    }
+    if (chars >= 0)
+    {
+        *dest += chars;
+        *dest_size -= chars;
+        *current_offset += chars;
+    }
+    return chars;
+}
+
 int serialize_node(struct kowhai_node_t** desc, void* data, int data_size, int* data_offset, char* target_buffer, size_t target_size, int level, kowhai_get_symbol_name_t get_name)
 {
     int target_offset = 0;
@@ -33,10 +83,9 @@ int serialize_node(struct kowhai_node_t** desc, void* data, int data_size, int* 
 
         for (i = 0; i < level; i++)
         {
-            if (snprintf(target_buffer, target_size, "\t") == 1);
-            target_buffer++;
-            target_offset++;
-            target_size--;
+            chars = add_string(&target_buffer, &target_size, &target_offset, "\t");
+            if (chars < 0)
+                return chars;
         }
 
         //
@@ -62,12 +111,9 @@ int serialize_node(struct kowhai_node_t** desc, void* data, int data_size, int* 
             {
                 char* children_str = ", \"children\": [\n";
                 char* end_str = "]}\n";
-                chars = snprintf(target_buffer, target_size, children_str);
+                chars = add_string(&target_buffer, &target_size, &target_offset, children_str);
                 if (chars < 0)
                     return chars;
-                target_offset += chars;
-                target_buffer += chars;
-                target_size -= chars;
                 (*desc) += 1;
                 chars = serialize_node(desc, data, data_size, data_offset, target_buffer, target_size, level + 1, get_name);
                 if (chars < 0)
@@ -77,59 +123,55 @@ int serialize_node(struct kowhai_node_t** desc, void* data, int data_size, int* 
                 target_size -= chars;
                 for (i = 0; i < level; i++)
                 {
-                    if (snprintf(target_buffer, target_size, "\t") == 1);
-                    target_buffer++;
-                    target_offset++;
-                    target_size--;
+                    chars = add_string(&target_buffer, &target_size, &target_offset, "\t");
+                    if (chars < 0)
+                        return chars;
                 }
-                chars = snprintf(target_buffer, target_size, end_str);
+                chars = add_string(&target_buffer, &target_size, &target_offset, end_str);
                 if (chars < 0)
                     return chars;
-                target_offset += chars;
-                target_buffer += chars;
-                target_size -= chars;
                 break;
             }
             default:
             {
-                char* value_str = ", \"value\": %d }\n";
-                char* value_str_float = ", \"value\": %f }\n";
-                int value_size;
-                switch (node->type)
-                {
-                    case KOW_INT8:
-                        chars = snprintf(target_buffer, target_size, value_str, *((int8_t*)data));
-                        break;
-                    case KOW_INT16:
-                        chars = snprintf(target_buffer, target_size, value_str, *((int16_t*)data));
-                        break;
-                    case KOW_INT32:
-                        chars = snprintf(target_buffer, target_size, value_str, *((int32_t*)data));
-                        break;
-                    case KOW_UINT8:
-                        chars = snprintf(target_buffer, target_size, value_str, *((uint8_t*)data));
-                        break;
-                    case KOW_UINT16:
-                        chars = snprintf(target_buffer, target_size, value_str, *((uint16_t*)data));
-                        break;
-                    case KOW_UINT32:
-                        chars = snprintf(target_buffer, target_size, value_str, *((uint32_t*)data));
-                        break;
-                    case KOW_FLOAT:
-                        chars = snprintf(target_buffer, target_size, value_str_float, *((float*)data));
-                        break;
-                    default:
-                        return -1;
-                }
+                int value_size = kowhai_get_node_type_size(node->type);
+                chars = add_string(&target_buffer, &target_size, &target_offset, ", \"value\": ");
                 if (chars < 0)
                     return chars;
-                target_offset += chars;
-                target_buffer += chars;
-                target_size -= chars;
-
-                value_size = kowhai_get_node_type_size(node->type);
-                data = (char*)data + value_size;
-                *data_offset += value_size;
+                if (node->count > 1)
+                {
+                    chars = add_string(&target_buffer, &target_size, &target_offset, "[");
+                    if (chars < 0)
+                        return chars;
+                    for (i = 0; i < node->count; i++)
+                    {
+                        chars = add_value(&target_buffer, &target_size, &target_offset, node->type, data);
+                        if (chars < 0)
+                            return chars;
+                        data = (char*)data + value_size;
+                        *data_offset += value_size;
+                        if (i < node->count - 1)
+                        {
+                            chars = add_string(&target_buffer, &target_size, &target_offset, ", ");
+                            if (chars < 0)
+                                return chars;
+                        }
+                    }
+                    chars = add_string(&target_buffer, &target_size, &target_offset, "]");
+                    if (chars < 0)
+                        return chars;
+                }
+                else
+                {
+                    chars = add_value(&target_buffer, &target_size, &target_offset, node->type, data);
+                    if (chars < 0)
+                        return chars;
+                    data = (char*)data + value_size;
+                    *data_offset += value_size;
+                }
+                chars = add_string(&target_buffer, &target_size, &target_offset, " }\n");
+                if (chars < 0)
+                    return chars;
                 break;
             }
         }
