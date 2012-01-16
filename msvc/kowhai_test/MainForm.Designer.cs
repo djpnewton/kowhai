e99b83e374e63f7045ba1b1e2414cb49c6d675f2
Oliver Thane <othane@gmail.com>
2012-01-16 16:16:13 +1300

    Revert "Merge branch 'kowhai_test_ui_improvements' into strings"
    This reverts commit f3c1f9bcf83f1540413210c7d2e88399d8a5db98, reversing
    changes made to 1bacc252df23188c125df367c8a61aa0e26de682.

--- msvc/kowhai_test/MainForm.Designer.cs
+++ msvc/kowhai_test/MainForm.Designer.cs
@@ -28,195 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.splitContainer5 = new System.Windows.Forms.SplitContainer();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.btnRefreshTrees = new System.Windows.Forms.ToolStripButton();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.splitContainer4 = new System.Windows.Forms.SplitContainer();
-            this.kowhaiTreeSettings = new kowhai_sharp.KowhaiTree();
-            this.kowhaiTreeShadow = new kowhai_sharp.KowhaiTree();
-            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
-            this.kowhaiTreeActions = new kowhai_sharp.KowhaiTree();
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.btnRefreshTrees = new System.Windows.Forms.Button();
             this.kowhaiTreeScope = new kowhai_sharp.KowhaiTree();
+            this.kowhaiTreeActions = new kowhai_sharp.KowhaiTree();
+            this.kowhaiTreeShadow = new kowhai_sharp.KowhaiTree();
+            this.kowhaiTreeSettings = new kowhai_sharp.KowhaiTree();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).BeginInit();
-            this.splitContainer5.Panel1.SuspendLayout();
-            this.splitContainer5.Panel2.SuspendLayout();
-            this.splitContainer5.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).BeginInit();
-            this.splitContainer4.Panel1.SuspendLayout();
-            this.splitContainer4.Panel2.SuspendLayout();
-            this.splitContainer4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
-            this.splitContainer3.Panel1.SuspendLayout();
-            this.splitContainer3.Panel2.SuspendLayout();
-            this.splitContainer3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.Panel2.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // splitContainer5
-            // 
-            this.splitContainer5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer5.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer5.IsSplitterFixed = true;
-            this.splitContainer5.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer5.Name = "splitContainer5";
-            this.splitContainer5.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer5.Panel1
-            // 
-            this.splitContainer5.Panel1.Controls.Add(this.toolStrip1);
-            // 
-            // splitContainer5.Panel2
-            // 
-            this.splitContainer5.Panel2.Controls.Add(this.splitContainer1);
-            this.splitContainer5.Size = new System.Drawing.Size(918, 523);
-            this.splitContainer5.SplitterDistance = 25;
-            this.splitContainer5.SplitterWidth = 1;
-            this.splitContainer5.TabIndex = 8;
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnRefreshTrees});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(918, 25);
-            this.toolStrip1.TabIndex = 8;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
             // btnRefreshTrees
             // 
-            this.btnRefreshTrees.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnRefreshTrees.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRefreshTrees.Location = new System.Drawing.Point(12, 12);
+            this.btnRefreshTrees.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRefreshTrees.Name = "btnRefreshTrees";
-            this.btnRefreshTrees.Size = new System.Drawing.Size(81, 21);
+            this.btnRefreshTrees.Size = new System.Drawing.Size(116, 34);
+            this.btnRefreshTrees.TabIndex = 0;
             this.btnRefreshTrees.Text = "Refresh Trees";
+            this.btnRefreshTrees.UseVisualStyleBackColor = true;
             this.btnRefreshTrees.Click += new System.EventHandler(this.btnRefreshTrees_Click);
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.splitContainer4);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.splitContainer3);
-            this.splitContainer1.Size = new System.Drawing.Size(918, 497);
-            this.splitContainer1.SplitterDistance = 528;
-            this.splitContainer1.TabIndex = 7;
-            // 
-            // splitContainer4
-            // 
-            this.splitContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer4.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer4.Name = "splitContainer4";
-            // 
-            // splitContainer4.Panel1
-            // 
-            this.splitContainer4.Panel1.Controls.Add(this.kowhaiTreeSettings);
-            // 
-            // splitContainer4.Panel2
-            // 
-            this.splitContainer4.Panel2.Controls.Add(this.kowhaiTreeShadow);
-            this.splitContainer4.Size = new System.Drawing.Size(528, 497);
-            this.splitContainer4.SplitterDistance = 267;
-            this.splitContainer4.TabIndex = 8;
-            // 
-            // kowhaiTreeSettings
-            // 
-            this.kowhaiTreeSettings.ContextMenuEnabled = true;
-            this.kowhaiTreeSettings.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.kowhaiTreeSettings.Location = new System.Drawing.Point(0, 0);
-            this.kowhaiTreeSettings.Margin = new System.Windows.Forms.Padding(2);
-            this.kowhaiTreeSettings.Name = "kowhaiTreeSettings";
-            this.kowhaiTreeSettings.Size = new System.Drawing.Size(267, 497);
-            this.kowhaiTreeSettings.TabIndex = 2;
-            // 
-            // kowhaiTreeShadow
-            // 
-            this.kowhaiTreeShadow.ContextMenuEnabled = true;
-            this.kowhaiTreeShadow.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.kowhaiTreeShadow.Location = new System.Drawing.Point(0, 0);
-            this.kowhaiTreeShadow.Margin = new System.Windows.Forms.Padding(2);
-            this.kowhaiTreeShadow.Name = "kowhaiTreeShadow";
-            this.kowhaiTreeShadow.Size = new System.Drawing.Size(257, 497);
-            this.kowhaiTreeShadow.TabIndex = 3;
-            // 
-            // splitContainer3
-            // 
-            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer3.Name = "splitContainer3";
-            // 
-            // splitContainer3.Panel1
-            // 
-            this.splitContainer3.Panel1.Controls.Add(this.kowhaiTreeActions);
-            // 
-            // splitContainer3.Panel2
-            // 
-            this.splitContainer3.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer3.Size = new System.Drawing.Size(386, 497);
-            this.splitContainer3.SplitterDistance = 164;
-            this.splitContainer3.TabIndex = 8;
-            // 
-            // kowhaiTreeActions
-            // 
-            this.kowhaiTreeActions.ContextMenuEnabled = true;
-            this.kowhaiTreeActions.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.kowhaiTreeActions.Location = new System.Drawing.Point(0, 0);
-            this.kowhaiTreeActions.Margin = new System.Windows.Forms.Padding(2);
-            this.kowhaiTreeActions.Name = "kowhaiTreeActions";
-            this.kowhaiTreeActions.Size = new System.Drawing.Size(164, 497);
-            this.kowhaiTreeActions.TabIndex = 4;
-            // 
-            // splitContainer2
-            // 
-            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Name = "splitContainer2";
-            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer2.Panel1
-            // 
-            this.splitContainer2.Panel1.Controls.Add(this.kowhaiTreeScope);
-            // 
-            // splitContainer2.Panel2
-            // 
-            this.splitContainer2.Panel2.Controls.Add(this.chart1);
-            this.splitContainer2.Size = new System.Drawing.Size(218, 497);
-            this.splitContainer2.SplitterDistance = 333;
-            this.splitContainer2.TabIndex = 9;
             // 
             // kowhaiTreeScope
             // 
-            this.kowhaiTreeScope.ContextMenuEnabled = true;
-            this.kowhaiTreeScope.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.kowhaiTreeScope.Location = new System.Drawing.Point(0, 0);
-            this.kowhaiTreeScope.Margin = new System.Windows.Forms.Padding(2);
+            this.kowhaiTreeScope.Location = new System.Drawing.Point(726, 52);
+            this.kowhaiTreeScope.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.kowhaiTreeScope.Name = "kowhaiTreeScope";
-            this.kowhaiTreeScope.Size = new System.Drawing.Size(218, 333);
-            this.kowhaiTreeScope.TabIndex = 8;
+            this.kowhaiTreeScope.Size = new System.Drawing.Size(233, 403);
+            this.kowhaiTreeScope.TabIndex = 4;
+            // 
+            // kowhaiTreeActions
+            // 
+            this.kowhaiTreeActions.Location = new System.Drawing.Point(487, 52);
+            this.kowhaiTreeActions.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.kowhaiTreeActions.Name = "kowhaiTreeActions";
+            this.kowhaiTreeActions.Size = new System.Drawing.Size(233, 511);
+            this.kowhaiTreeActions.TabIndex = 3;
+            // 
+            // kowhaiTreeShadow
+            // 
+            this.kowhaiTreeShadow.Location = new System.Drawing.Point(250, 52);
+            this.kowhaiTreeShadow.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.kowhaiTreeShadow.Name = "kowhaiTreeShadow";
+            this.kowhaiTreeShadow.Size = new System.Drawing.Size(231, 511);
+            this.kowhaiTreeShadow.TabIndex = 2;
+            // 
+            // kowhaiTreeSettings
+            // 
+            this.kowhaiTreeSettings.Location = new System.Drawing.Point(12, 52);
+            this.kowhaiTreeSettings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.kowhaiTreeSettings.Name = "kowhaiTreeSettings";
+            this.kowhaiTreeSettings.Size = new System.Drawing.Size(232, 511);
+            this.kowhaiTreeSettings.TabIndex = 1;
             // 
             // chart1
             // 
@@ -224,52 +88,32 @@
             chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.chart1.Location = new System.Drawing.Point(0, 0);
-            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Location = new System.Drawing.Point(726, 460);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(218, 160);
-            this.chart1.TabIndex = 6;
+            this.chart1.Size = new System.Drawing.Size(233, 100);
+            this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 523);
-            this.Controls.Add(this.splitContainer5);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.ClientSize = new System.Drawing.Size(966, 572);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.kowhaiTreeScope);
+            this.Controls.Add(this.kowhaiTreeActions);
+            this.Controls.Add(this.kowhaiTreeShadow);
+            this.Controls.Add(this.kowhaiTreeSettings);
+            this.Controls.Add(this.btnRefreshTrees);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             this.Load += new System.EventHandler(this.MainForm_Load);
-            this.splitContainer5.Panel1.ResumeLayout(false);
-            this.splitContainer5.Panel1.PerformLayout();
-            this.splitContainer5.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).EndInit();
-            this.splitContainer5.ResumeLayout(false);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.splitContainer4.Panel1.ResumeLayout(false);
-            this.splitContainer4.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).EndInit();
-            this.splitContainer4.ResumeLayout(false);
-            this.splitContainer3.Panel1.ResumeLayout(false);
-            this.splitContainer3.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
-            this.splitContainer3.ResumeLayout(false);
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-            this.splitContainer2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
@@ -277,17 +121,10 @@
 
         #endregion
 
-        private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.SplitContainer splitContainer5;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton btnRefreshTrees;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.SplitContainer splitContainer4;
+        private System.Windows.Forms.Button btnRefreshTrees;
         private kowhai_sharp.KowhaiTree kowhaiTreeSettings;
         private kowhai_sharp.KowhaiTree kowhaiTreeShadow;
-        private System.Windows.Forms.SplitContainer splitContainer3;
         private kowhai_sharp.KowhaiTree kowhaiTreeActions;
-        private System.Windows.Forms.SplitContainer splitContainer2;
         private kowhai_sharp.KowhaiTree kowhaiTreeScope;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
