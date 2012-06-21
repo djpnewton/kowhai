ee2ffc1f3d3e42be1dcb539a4c14ee730a3e12a5
Daniel Newton <djpnewton@gmail.com>
2012-06-22 11:54:17 +1200

    added hidapi python wrapper

--- msvc/kowhai_python/Program.py
+++ msvc/kowhai_python/Program.py
@@ -4,5 +4,20 @@ sys.path.append("../../python")
 import kowhai
 import kowhai_protocol
 
+import hidapi
+
 #TODO - more stuff!
 print kowhai.kowhai_lib
+
+orig_devinfo = devinfo = hidapi.HID.enumerate(0, 0)
+
+while devinfo:
+    print "Path:      ", devinfo.contents.path
+    print "Vendor Id: ", hex(devinfo.contents.vendor_id)
+    print "Product Id:", hex(devinfo.contents.product_id)
+    print "Usage Page:", devinfo.contents.usage_page
+    print "Usage:     ", devinfo.contents.usage
+    print
+    devinfo = devinfo.contents.next
+
+hidapi.HID.free_enumeration(orig_devinfo)
